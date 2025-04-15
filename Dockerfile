95854cbeb0c3e9bf7224d7daec67d0263c0e7f2e
g1ok3r <gioker.tech@gmail.com>
2025-04-15 18:58:04 -0500

    fixing app

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
-FROM public.ecr.aws/sam/build-python3.9:latest
+FROM public.ecr.aws/docker/library/python:3.13
 
 WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
-
-# Copiar el archivo .proto y compilarlo
-COPY proto/image_processor.proto .
-RUN python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. image_processor.proto
 
 COPY . .
 
--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from concurrent import futures
 import grpc
-import app_b_pb2
-import app_b_pb2_grpc
+import image_processor_pb2
+import image_processor_pb2_grpc
 from PIL import Image
 import io
 
-class ImageProcessor(app_b_pb2_grpc.ImageProcessorServicer):
+class ImageProcessor(image_processor_pb2_grpc.ImageProcessorServicer):
     def ProcessImage(self, request, context):
         image = Image.open(io.BytesIO(request.image))
         # Apply transformations (resize, blur, rotate)
@@ -13,14 +13,17 @@ class ImageProcessor(app_b_pb2_grpc.ImageProcessorServicer):
         image = image.rotate(45)
         output = io.BytesIO()
         image.save(output, format='JPEG')
-        return app_b_pb2.ImageResponse(image=output.getvalue())
+        return image_processor_pb2.ImageResponse(image=output.getvalue())
 
 def serve():
+    print("gRPC Server Starting")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    app_b_pb2_grpc.add_ImageProcessorServicer_to_server(ImageProcessor(), server)
+    image_processor_pb2_grpc.add_ImageProcessorServicer_to_server(ImageProcessor(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
+    print("gRPC Server started")
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
